b3380e4df742522c5686269327cb7a71b9964243
mr-santos <mr-santos@mr-santos.mr-santos>
2024-04-09 10:15:25 -0300

    importando modulo de splash

---
+++ lib/app/modules/splash/splash_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import '../../constants/app_colors.dart';
+import '../../constants/app_images.dart';
+import '../../routes/app_routes.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+      const Duration(milliseconds: 2000),
+      () {
+        Modular.to.navigate(AppRoutes.homeModule);//${AppRoutes.createUser}'); // vai ser home
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.primaryColor,
+      body: Center(child: Image.asset(AppImages.logo)),
+    );
+  }
+}
